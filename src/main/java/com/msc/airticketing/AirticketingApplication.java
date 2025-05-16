63e2e1c5244d0d66f76fdaca998c9537a6f0211b
Niroshan Arachchige <niroshansembakutti@gmail.com>
2025-05-16 15:40:20 +0530

    Initial Commit

---
+++ src/main/java/com/msc/airticketing/AirticketingApplication.java
@@ -0,0 +1,13 @@
+package com.msc.airticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirticketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirticketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msc/airticketing/AirticketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msc.airticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirticketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
